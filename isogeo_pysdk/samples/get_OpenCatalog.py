d0829542fc4cb5f74a43550107aa45358dea54bd
GeoJulien <Guts@users.noreply.github.com>
2016-05-17 15:52:59 +0200

    add sample

---
+++ isogeo_pysdk/samples/get_OpenCatalog.py
@@ -0,0 +1,63 @@
+# -*- coding: UTF-8 -*-
+#!/usr/bin/env python
+from __future__ import (absolute_import, print_function, unicode_literals)
+# ------------------------------------------------------------------------------
+# Name:         Isogeo sample - Get OpenCatalog if exists in shares
+# Purpose:      Get the latest modified datasets from an Isogeo share, using
+#               the Isogeo API Python minimalist SDK.
+# Author:       Julien Moura (@geojulien)
+#
+# Python:       2.7.x
+# Created:      14/02/2016
+# Updated:      18/02/2016
+# ------------------------------------------------------------------------------
+
+# ##############################################################################
+# ########## Libraries #############
+# ##################################
+
+# Standard library
+import ConfigParser     # to manage options.ini
+from os import path
+
+# 3rd party library
+from dateutil.parser import parse as dtparse
+
+# Isogeo
+from isogeo_pysdk import Isogeo
+
+# ############################################################################
+# ######### Main program ###########
+# ##################################
+
+# storing application parameters into an ini file
+settings_file = r"../isogeo_params.ini"
+
+# testing ini file
+if not path.isfile(path.realpath(settings_file)):
+    print("ERROR: to execute this script as standalone, you need to store your Isogeo application settings in a isogeo_params.ini file. You can use the template to set your own.")
+    import sys
+    sys.exit()
+else:
+    pass
+
+# reading ini file
+config = ConfigParser.SafeConfigParser()
+config.read(settings_file)
+
+share_id = config.get('auth', 'app_id')
+share_token = config.get('auth', 'app_secret')
+
+# ------------ Real start ----------------
+# instanciating the class
+isogeo = Isogeo(client_id=share_id,
+                client_secret=share_token,
+                lang="fr")
+
+token = isogeo.connect()
+
+# ------------ REAL START ----------------------------
+share = isogeo.share(token, "http")
+print(share)
+print(len(share))
+print(share[0].keys(), share[0].get("applications"))
