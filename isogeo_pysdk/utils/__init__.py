8d671633fb01cb26ddf074cff394a58177bd36ce
GeoJulien <julien.moura+dev@gmail.com>
2016-12-22 10:07:57 +0100

    remove utils subfolder to keep a simple structure

--- isogeo_pysdk/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-﻿#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""
-    This module containe utils scripts to help Isogeo API main class
-"""
-
-from api_strings_translator import IsogeoTranslator
