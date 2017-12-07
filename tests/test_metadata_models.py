ffc0875c0b804cf6ea747cbfbfb8347ca30aee9c
Guts <Guts@users.noreply.github.com>
2017-12-07 17:28:53 +0100

    add resource, series and service models testing

--- tests/test_metadata_models.py
+++ tests/test_metadata_models.py
@@ -168,7 +168,182 @@ class Search(unittest.TestCase):
         self.assertIn("owner:32f7e95ec4e94ca3bc1afda960003882", md_tags)
         self.assertIn("provider:manual", md_tags)
 
+    def test_search_result_resource_min(self):
+        """Search with sub-resources included."""
+        # from a specific md. Can be viewed here: https://goo.gl/RDWDWJ
+        search = self.isogeo.search(self.bearer, whole_share=0,
+                                    specific_md=["d3df351cb9a64a35b6bde4a383c9bddc",])
+        md = search.get("results")[0]
+        self.assertIsInstance(md, dict)
 
+        # basic metadata keys/keys
+        self.assertIn("_id", md)
+        self.assertIn("_created", md)
+        self.assertIn("_modified", md)
+        self.assertIn("_creator", md)
+        self.assertIn("_abilities", md)
+        self.assertIn("title", md)        
+        self.assertIn("type", md)
+        self.assertIn("tags", md)
+        self.assertIn("editionProfile", md)
+        
+        # _creator keys/values
+        md_crea = md.get("_creator")
+        self.assertIsInstance(md_crea, dict)
+        self.assertIn("_id", md_crea)
+        self.assertIn("_tag", md_crea)
+        self.assertIn("_created", md_crea)
+        self.assertIn("_modified", md_crea)
+        self.assertIn("contact", md_crea)
+        self.assertIn("canCreateMetadata", md_crea)
+        self.assertIn("canCreateLegacyServiceLinks", md_crea)
+        self.assertIn("areKeywordsRestricted", md_crea)
+        self.assertIn("hasCswClient", md_crea)
+        self.assertIn("hasScanFme", md_crea)
+        self.assertIn("keywordsCasing", md_crea)
+        self.assertIn("metadataLanguage", md_crea)
+
+        # _creator subcontact keys/values
+        md_crea_ct = md_crea.get("contact")
+        self.assertIsInstance(md_crea_ct, dict)
+        self.assertIn("_id", md_crea_ct)
+        self.assertIn("_tag", md_crea_ct)
+        self.assertIn("_deleted", md_crea_ct)
+        self.assertIn("type", md_crea_ct)
+        self.assertIn("name", md_crea_ct)
+        self.assertIn("email", md_crea_ct)
+        self.assertIn("addressLine1", md_crea_ct)
+        self.assertIn("addressLine2", md_crea_ct)
+        self.assertIn("city", md_crea_ct)
+        self.assertIn("zipCode", md_crea_ct)
+        self.assertIn("countryCode", md_crea_ct)
+        self.assertIn("available", md_crea_ct)
+
+        # _tags keys/values
+        md_tags = md.get("tags")
+        self.assertIsInstance(md_tags, dict)
+        self.assertIn("type:resource", md_tags)
+        self.assertIn("owner:32f7e95ec4e94ca3bc1afda960003882", md_tags)
+        self.assertIn("provider:manual", md_tags)
+
+    def test_search_result_series_min(self):
+        """Search with sub-resources included."""
+        # from a specific md. Can be viewed here: https://goo.gl/RDWDWJ
+        search = self.isogeo.search(self.bearer, whole_share=0,
+                                    specific_md=["af522449ae5041cf9f058c9e8822f45f",])
+        md = search.get("results")[0]
+        self.assertIsInstance(md, dict)
+
+        # basic metadata keys/keys
+        self.assertIn("_id", md)
+        self.assertIn("_created", md)
+        self.assertIn("_modified", md)
+        self.assertIn("_creator", md)
+        self.assertIn("_abilities", md)
+        self.assertIn("title", md)
+        self.assertIn("type", md)
+        self.assertIn("tags", md)
+        self.assertIn("editionProfile", md)
+        self.assertIn("series", md)
+        self.assertEqual(md.get("series"), 1)
+        
+        # _creator keys/values
+        md_crea = md.get("_creator")
+        self.assertIsInstance(md_crea, dict)
+        self.assertIn("_id", md_crea)
+        self.assertIn("_tag", md_crea)
+        self.assertIn("_created", md_crea)
+        self.assertIn("_modified", md_crea)
+        self.assertIn("contact", md_crea)
+        self.assertIn("canCreateMetadata", md_crea)
+        self.assertIn("canCreateLegacyServiceLinks", md_crea)
+        self.assertIn("areKeywordsRestricted", md_crea)
+        self.assertIn("hasCswClient", md_crea)
+        self.assertIn("hasScanFme", md_crea)
+        self.assertIn("keywordsCasing", md_crea)
+        self.assertIn("metadataLanguage", md_crea)
+
+        # _creator subcontact keys/values
+        md_crea_ct = md_crea.get("contact")
+        self.assertIsInstance(md_crea_ct, dict)
+        self.assertIn("_id", md_crea_ct)
+        self.assertIn("_tag", md_crea_ct)
+        self.assertIn("_deleted", md_crea_ct)
+        self.assertIn("type", md_crea_ct)
+        self.assertIn("name", md_crea_ct)
+        self.assertIn("email", md_crea_ct)
+        self.assertIn("addressLine1", md_crea_ct)
+        self.assertIn("addressLine2", md_crea_ct)
+        self.assertIn("city", md_crea_ct)
+        self.assertIn("zipCode", md_crea_ct)
+        self.assertIn("countryCode", md_crea_ct)
+        self.assertIn("available", md_crea_ct)
+
+        # _tags keys/values
+        md_tags = md.get("tags")
+        self.assertIsInstance(md_tags, dict)
+        self.assertIn("type:raster-dataset", md_tags)
+        self.assertIn("owner:32f7e95ec4e94ca3bc1afda960003882", md_tags)
+        self.assertIn("provider:manual", md_tags)
+
+    def test_search_result_service_min(self):
+        """Search with sub-resources included."""
+        # from a specific md. Can be viewed here: https://goo.gl/RDWDWJ
+        search = self.isogeo.search(self.bearer, whole_share=0,
+                                    specific_md=["a7653d11752c4a6c890a9f9d1603eceb",])
+        md = search.get("results")[0]
+        self.assertIsInstance(md, dict)
+
+        # basic metadata keys/keys
+        self.assertIn("_id", md)
+        self.assertIn("_created", md)
+        self.assertIn("_modified", md)
+        self.assertIn("_creator", md)
+        self.assertIn("_abilities", md)
+        self.assertIn("title", md)
+        self.assertIn("path", md)
+        self.assertIn("type", md)
+        self.assertIn("tags", md)
+        self.assertIn("editionProfile", md)
+        
+        # _creator keys/values
+        md_crea = md.get("_creator")
+        self.assertIsInstance(md_crea, dict)
+        self.assertIn("_id", md_crea)
+        self.assertIn("_tag", md_crea)
+        self.assertIn("_created", md_crea)
+        self.assertIn("_modified", md_crea)
+        self.assertIn("contact", md_crea)
+        self.assertIn("canCreateMetadata", md_crea)
+        self.assertIn("canCreateLegacyServiceLinks", md_crea)
+        self.assertIn("areKeywordsRestricted", md_crea)
+        self.assertIn("hasCswClient", md_crea)
+        self.assertIn("hasScanFme", md_crea)
+        self.assertIn("keywordsCasing", md_crea)
+        self.assertIn("metadataLanguage", md_crea)
+
+        # _creator subcontact keys/values
+        md_crea_ct = md_crea.get("contact")
+        self.assertIsInstance(md_crea_ct, dict)
+        self.assertIn("_id", md_crea_ct)
+        self.assertIn("_tag", md_crea_ct)
+        self.assertIn("_deleted", md_crea_ct)
+        self.assertIn("type", md_crea_ct)
+        self.assertIn("name", md_crea_ct)
+        self.assertIn("email", md_crea_ct)
+        self.assertIn("addressLine1", md_crea_ct)
+        self.assertIn("addressLine2", md_crea_ct)
+        self.assertIn("city", md_crea_ct)
+        self.assertIn("zipCode", md_crea_ct)
+        self.assertIn("countryCode", md_crea_ct)
+        self.assertIn("available", md_crea_ct)
+
+        # _tags keys/values
+        md_tags = md.get("tags")
+        self.assertIsInstance(md_tags, dict)
+        self.assertIn("type:service", md_tags)
+        self.assertIn("owner:32f7e95ec4e94ca3bc1afda960003882", md_tags)
+        self.assertIn("provider:manual", md_tags)
 
 # ##############################################################################
 # ##### Stand alone program ########
