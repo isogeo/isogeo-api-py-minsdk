6f42e90d7733b98587ff745e5a67adcb44b3a498
Guts <Guts@users.noreply.github.com>
2018-09-05 15:11:40 +0200

    bump to version 2.20.4

--- isogeo_pysdk/isogeo_sdk.py
+++ isogeo_pysdk/isogeo_sdk.py
@@ -41,7 +41,7 @@ except (ImportError, ValueError, SystemError):
 
 checker = checker.IsogeoChecker()
 utils = utils.IsogeoUtils()
-version = "2.20.3"
+version = "2.20.4"
 
 # #############################################################################
 # ########## Classes ###############
