0c20ef5031aa290c99afd69bfda0c430c93d83a0
Guts <julien.moura+dev@isogeo.fr>
2019-04-02 15:46:07 +0200

    modernize sample

--- isogeo_pysdk/samples/export_batch_xml19139.py
+++ isogeo_pysdk/samples/export_batch_xml19139.py
@@ -1,14 +1,14 @@
 # -*- coding: UTF-8 -*-
-#!/usr/bin/env python
-from __future__ import (absolute_import, print_function, unicode_literals)
+#! python3
+
 # ------------------------------------------------------------------------------
 # Name:         Isogeo sample - Batch export to XML ISO19139
 # Purpose:      Exports each of 10 last updated metadata into an XML ISO19139
-# Author:       Julien Moura (@geojulien)
+# Author:       Isogeo
 #
-# Python:       2.7.x
+# Python:       3.5+
 # Created:      14/11/2016
-# Updated:      15/11/2016
+# Updated:      15/04/2019
 # ------------------------------------------------------------------------------
 
 # ##############################################################################
@@ -16,11 +16,20 @@ from __future__ import (absolute_import, print_function, unicode_literals)
 # ##################################
 
 # Standard library
-import configparser     # to manage options.ini
-from os import path
+from os import environ
+from pathlib import Path
+from timeit import default_timer
 
 # Isogeo
 from isogeo_pysdk import Isogeo
+
+# #############################################################################
+# ########## Globals ###############
+# ##################################
+
+# required subfolders
+out_dir = Path("_output/")
+out_dir.mkdir(exist_ok=True)
 
 # ############################################################################
 # ######### Main program ###########
@@ -28,34 +37,21 @@ from isogeo_pysdk import Isogeo
 
 if __name__ == '__main__':
     """Standalone execution"""
-    # storing application parameters into an ini file
-    settings_file = r"../isogeo_params.ini"
+    chrono_start = default_timer()  # chrono
 
-    # testing ini file
-    if not path.isfile(path.realpath(settings_file)):
-        print("ERROR: to execute this script as standalone, you need to store your Isogeo application settings in a isogeo_params.ini file. You can use the template to set your own.")
-        import sys
-        sys.exit()
-    else:
-        pass
+    # Authentication #########
+    app_id = environ.get('ISOGEO_API_DEV_ID')
+    app_token = environ.get('ISOGEO_API_DEV_SECRET')
 
-    # reading ini file
-    config = configparser.SafeConfigParser()
-    config.read(settings_file)
+    # start Isogeo
+    isogeo = Isogeo(client_id=app_id,
+                    client_secret=app_token
+                    )
 
-    share_id = config.get('auth', 'app_id')
-    share_token = config.get('auth', 'app_secret')
-
-    # ------------ Real start ----------------
-    # instanciating the class
-    isogeo = Isogeo(client_id=share_id,
-                    client_secret=share_token,
-                    lang="fr")
-
+    # getting a token
     token = isogeo.connect()
 
-    # ------------ REAL START ----------------------------
-
+    # Process #########
     latest_data_modified = isogeo.search(token,
                                          page_size=10,
                                          order_by="modified",
@@ -67,6 +63,10 @@ if __name__ == '__main__':
         xml_stream = isogeo.xml19139(token,
                                      md.get("_id"))
 
-        with open("{}.xml".format(title), 'wb') as fd:
+        with open(out_dir / "{}.xml".format(title), 'wb') as fd:
             for block in xml_stream.iter_content(1024):
                 fd.write(block)
+
+    # chrono
+    chrono_end = default_timer()
+    print("Done in: {:5.2f}s".format(chrono_end - chrono_start))
