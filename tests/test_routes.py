5761be3a462a59b70aecc6ac0cb265b7e79369bf
Guts <julien.moura+dev@isogeo.fr>
2019-04-11 14:18:47 +0200

    adapt unit test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,6 +1,17 @@
 # -*- coding: UTF-8 -*-
 #! python3
 
+"""
+    Usage from the repo root folder:
+
+    ```python
+    # for whole test
+    python -m unittest tests.test_routes
+    # for specific
+    python -m unittest tests.test_routes.TestRoutes.test_resources_search
+    ```
+"""
+
 # #############################################################################
 # ########## Libraries #############
 # ##################################
@@ -42,7 +53,7 @@ class TestRoutes(unittest.TestCase):
     def setUp(self):
         """Executed before each test."""
         self.isogeo = Isogeo(client_id=app_id, client_secret=app_token)
-        self.bearer = self.isogeo.connect()
+        self.isogeo.connect()
 
     def tearDown(self):
         """Executed after each test."""
@@ -51,25 +62,23 @@ class TestRoutes(unittest.TestCase):
     # -- ALL APPS ------------------------------------------------------------
     def test_resources_search(self):
         """GET :/resources/search and GET :/resources/{rid}"""
-        search = self.isogeo.search(self.bearer, page_size=1, whole_share=0, check=0)
+        search = self.isogeo.search(page_size=1, whole_share=0, check=0)
 
-        self.isogeo.resource(
-            self.bearer, id_resource=search.get("results")[0].get("_id")
-        )
+        self.isogeo.resource(id_resource=search.get("results")[0].get("_id"))
 
     def test_shares(self):
         """GET :/shares and GET :/shares/{sid}"""
-        shares = self.isogeo.shares(self.bearer)
-        self.isogeo.share(self.bearer, share_id=shares[0].get("_id"))
+        shares = self.isogeo.shares()
+        self.isogeo.share(share_id=shares[0].get("_id"))
 
     def test_thesauri(self):
         """GET :/thesauri and GET :/thesaurus/{tid}"""
-        thesauri = self.isogeo.thesauri(self.bearer)
-        self.isogeo.thesaurus(self.bearer, thez_id=thesauri[0].get("_id"))
+        thesauri = self.isogeo.thesauri()
+        self.isogeo.thesaurus(thez_id=thesauri[0].get("_id"))
 
     def test_licenses(self):
         """Try to get licenses details."""
-        search = self.isogeo.search(self.bearer, page_size=0, whole_share=0, check=0)
+        search = self.isogeo.search(page_size=0, whole_share=0, check=0)
         # get a workgroup id and a license within tags
         for tag in search.get("tags"):
             if tag.startswith("license:"):
@@ -83,13 +92,13 @@ class TestRoutes(unittest.TestCase):
                 pass
 
         # get workgroup licenses
-        licenses = self.isogeo.licenses(self.bearer, owner_id=wg)
+        licenses = self.isogeo.licenses(owner_id=wg)
         self.assertIsInstance(licenses, tuple)
         self.assertIn(401, licenses)
 
         # a specific license
         license = self.isogeo.license(
-            self.bearer, license_id="f6e0c665905a4feab1e9c1d6359a225f"
+            license_id="f6e0c665905a4feab1e9c1d6359a225f"
         )
         self.assertIsInstance(license, dict)
         self.assertIn("_id", license)
@@ -100,9 +109,9 @@ class TestRoutes(unittest.TestCase):
 
     def test_keywords(self):
         """GET :/thesauri/keywords"""
-        thez_id = self.isogeo.thesauri(self.bearer)[0].get("_id")
+        thez_id = self.isogeo.thesauri()[0].get("_id")
         # list tags
-        thez_keywords = self.isogeo.keywords(self.bearer, thez_id=thez_id, page_size=1)
+        thez_keywords = self.isogeo.keywords(thez_id=thez_id, page_size=1)
         self.assertIsInstance(thez_keywords, dict)
         self.assertIn("limit", thez_keywords)
         self.assertIn("offset", thez_keywords)
