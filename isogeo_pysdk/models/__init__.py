916c9689625882d66ef8a56d15c57bf91c635933
Guts <julien.moura+dev@isogeo.fr>
2019-05-01 17:19:07 +0200

    prevent flake8 warnings

--- isogeo_pysdk/models/__init__.py
+++ isogeo_pysdk/models/__init__.py
@@ -1,10 +1,10 @@
 # coding: utf-8
-#! python3
+#! python3  # noqa: E265
 
-from .catalog import Catalog
-from .contact import Contact
-from .event import Event
-from .link import Link
-from .specification import Specification
-from .workgroup import Workgroup
+from .catalog import Catalog  # noqa: F401
+from .contact import Contact  # noqa: F401
+from .event import Event  # noqa: F401
 from .license import License  # noqa: F401
+from .link import Link  # noqa: F401
+from .specification import Specification  # noqa: F401
+from .workgroup import Workgroup  # noqa: F401
