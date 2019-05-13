1640822a6d911a8703662a9ac3c8906993ac668d
Guts <julien.moura+dev@isogeo.fr>
2019-05-13 18:13:02 +0200

    improve resource test

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -17,20 +17,24 @@
 # ##################################
 
 # Standard library
-from os import environ
+import json
 import logging
+import unittest
+from os import environ
+from pathlib import Path
+from random import sample
 from socket import gethostname
 from sys import exit
 from time import gmtime, strftime
-import unittest
 
 # 3rd party
 from dotenv import load_dotenv
 from oauthlib.oauth2 import LegacyApplicationClient
+from requests import HTTPError
 
 # module target
-from isogeo_pysdk import IsogeoSession, __version__ as pysdk_version, Metadata
-
+from isogeo_pysdk import IsogeoSession, Metadata
+from isogeo_pysdk import __version__ as pysdk_version
 
 # #############################################################################
 # ######## Globals #################
@@ -48,6 +52,8 @@ platform = environ.get("ISOGEO_PLATFORM", "qa")
 user_email = environ.get("ISOGEO_USER_NAME")
 user_password = environ.get("ISOGEO_USER_PASSWORD")
 workgroup_test = environ.get("ISOGEO_WORKGROUP_TEST_UUID")
+
+MD_COMPLETE = environ.get("ISOGEO_FIXTURES_METADATA_COMPLETE")
 
 # #############################################################################
 # ########## Classes ###############
@@ -83,6 +89,9 @@ class TestMetadata(unittest.TestCase):
         # getting a token
         self.isogeo.connect(username=user_email, password=user_password)
 
+        # fixtures
+        self.local_metadata = [Path(r"tests/fixtures", "resource_complete_1.json")]
+
     def tearDown(self):
         """Executed after each test."""
         # clean created metadata
@@ -111,23 +120,47 @@ class TestMetadata(unittest.TestCase):
         # add created workgroup to deletion
         self.li_metadata_to_delete.append(my_new_metadata._id)
 
-    # def test_metadata_create_complete(self):
-    #     """POST :groups/}"""
-    #     # to create a workgroup, a contact is required
-    #     existing_wg = Workgroup(self.isogeo.workgroup(workgroup_id=workgroup_test))
-    #     new_wg = self.isogeo.workgroup_create(workgroup=existing_wg)
+    def test_metadata_complete(self):
+        """GET :/resources/{resource_uuid}}"""
+        # -- ONLINE -----------------------------
+        # get a complete metadata
+        try:
+            metadata_complete = self.isogeo.resource(
+                resource_id=MD_COMPLETE, include="all"
+            )
+        except HTTPError:
+            # retrieve workgroup metadata
+            wg_metadata = self.isogeo.workgroup_metadata(
+                workgroup_id=workgroup_test, page_size=30
+            )
+            # randomly pick one
+            rand_metadata_uuid = sample(wg_metadata.get("results"), 1)[0].get("_id")
+            metadata_complete = self.isogeo.resource(
+                resource_id=rand_metadata_uuid, include="all"
+            )
 
-    #     # checks
-    #     self.assertEqual(
-    #         new_wg.contact.get("name"), "TEST_UNIT_AUTO {}".format(self.discriminator)
-    #     )
-    #     # self.assertTrue(self.isogeo.workgroup_exists(new_wg.get("_id")))
+        # load it
+        md_test = Metadata(**metadata_complete)
+        # test object methods
+        self.assertIsInstance(md_test.to_dict(), dict)
+        self.assertIsInstance(md_test.to_dict_creation(), dict)
 
-    #     # add created workgroup to deletion
-    #     # self.li_metadata_to_delete.append(new_wg._id)
+        # -- OFFLINE -----------------------------
+        for local_md in self.local_metadata:
+            with local_md.open() as in_json:
+                md_loaded_from_json = json.loads(in_json.read())
+                # manually handle bad attributes
+                md_loaded_from_json["coordinateSystem"] = md_loaded_from_json.pop(
+                    "coordinate-system", list
+                )
+                md_loaded_from_json["featureAttributes"] = md_loaded_from_json.pop(
+                    "feature-attributes", list
+                )
+                # load it
+                Metadata(**md_loaded_from_json)
 
     def test_metadata_get_workgroup(self):
-        """GET :groups/{workgroup_uuid}/metadata}"""
+        """GET :groups/{workgroup_uuid}/resources/search}"""
         # retrieve workgroup metadata
         wg_metadata = self.isogeo.workgroup_metadata(
             workgroup_id=workgroup_test, page_size=30
@@ -135,8 +168,9 @@ class TestMetadata(unittest.TestCase):
         # parse and test object loader
         for i in wg_metadata.get("results"):
             metadata = Metadata(**i)
-            # tes object method
+            # test object methods
             self.assertIsInstance(metadata.to_dict(), dict)
+            self.assertIsInstance(metadata.to_dict_creation(), dict)
             # tests attributes structure
             # self.assertTrue(hasattr(ct, "_abilities"))
             # self.assertTrue(hasattr(ct, "_created"))
