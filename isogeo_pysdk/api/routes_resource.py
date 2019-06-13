dc6078d4b0c0217077d289588ff8f221136763fc
Guts <Guts@users.noreply.github.com>
2019-06-13 18:46:22 +0200

    add method to create a metadata

--- isogeo_pysdk/api/routes_resource.py
+++ isogeo_pysdk/api/routes_resource.py
@@ -103,6 +103,103 @@ class ApiResource:
         # end of method
         return Metadata(**metadata)
 
+    @ApiDecorators._check_bearer_validity
+    def create(
+        self, workgroup_id: str, metadata: Metadata, check_exists: int = 1
+    ) -> Metadata:
+        """Add a new metadata to a workgroup.
+
+        :param str workgroup_id: identifier of the owner workgroup
+        :param Metadata metadata: Metadata model object to create
+        :param int check_exists: check if a metadata with the same title already exists into the workgroup:
+
+        - 0 = no check
+        - 1 = compare name [DEFAULT]
+        """
+        # check workgroup UUID
+        if not checker.check_is_uuid(workgroup_id):
+            raise ValueError("Workgroup ID is not a correct UUID.")
+        else:
+            pass
+
+        # check if metadata already exists in workgroup
+        # if check_exists == 1:
+        #     # retrieve workgroup metadatas
+        #     if not self.api_client._wg_metadatas_names:
+        #         self.metadatas(workgroup_id=workgroup_id, include=[])
+        #     # check
+        #     if metadata.name in self.api_client._wg_metadatas_names:
+        #         logger.debug(
+        #             "Metadata with the same name already exists: {}. Use 'metadata_update' instead.".format(
+        #                 metadata.name
+        #             )
+        #         )
+        #         return False
+        # else:
+        #     pass
+
+        # build request url
+        url_metadata_create = utils.get_request_base_url(
+            route="groups/{}/resources".format(workgroup_id)
+        )
+
+        # request
+        req_new_metadata = self.api_client.post(
+            url=url_metadata_create,
+            json=metadata.to_dict_creation(),
+            headers=self.api_client.header,
+            proxies=self.api_client.proxies,
+            verify=self.api_client.ssl,
+            timeout=self.api_client.timeout,
+        )
+
+        # checking response
+        req_check = checker.check_api_response(req_new_metadata)
+        if isinstance(req_check, tuple):
+            return req_check
+
+        # load new metadata and save it to the cache
+        new_metadata = Metadata(**req_new_metadata)
+        # self.api_client._wg_metadatas_names[new_metadata.name] = new_metadata._id
+
+        # end of method
+        return new_metadata
+
+    @ApiDecorators._check_bearer_validity
+    def exists(self, resource_id: str) -> bool:
+        """Check if the specified resource exists and is available for the authenticated user.
+
+        :param str resource_id: identifier of the resource to verify
+        """
+        # check metadata UUID
+        if not checker.check_is_uuid(resource_id):
+            raise ValueError(
+                "Resource ID is not a correct UUID: {}".format(resource_id)
+            )
+        else:
+            pass
+
+        # URL builder
+        url_metadata_exists = "{}{}".format(
+            utils.get_request_base_url("resources"), resource_id
+        )
+
+        # request
+        req_metadata_exists = self.api_client.get(
+            url=url_metadata_exists,
+            headers=self.api_client.header,
+            proxies=self.api_client.proxies,
+            verify=self.api_client.ssl,
+            timeout=self.api_client.timeout,
+        )
+
+        # checking response
+        req_check = checker.check_api_response(req_metadata_exists)
+        if isinstance(req_check, tuple):
+            return req_check
+
+        return True
+
     # def workgroup_metadata(
     #     self,
     #     workgroup_id: str,
@@ -154,6 +251,8 @@ class ApiResource:
     #     #     self._wg_apps_names = {i.get("name"): i.get("_id") for i in wg_metadata}
 
     #     return wg_metadata
+
+    # -- Routes to manage the related objects ------------------------------------------
 
 
 # ##############################################################################
