ede8b4c2630a6a33ed78876122a7f7383ee29524
Guts <Guts@users.noreply.github.com>
2019-07-19 17:23:53 +0200

    pep8

--- docs/conf.py
+++ docs/conf.py
@@ -16,19 +16,24 @@ from datetime import date
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(r'..'))
-from isogeo_pysdk import (Isogeo, IsogeoChecker, IsogeoTranslator,
-                          IsogeoUtils, __version__)
+sys.path.insert(0, os.path.abspath(r".."))
+from isogeo_pysdk import (
+    Isogeo,
+    IsogeoChecker,
+    IsogeoTranslator,
+    IsogeoUtils,
+    __version__,
+)
 
 # -- Build environment -----------------------------------------------------
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-   
+on_rtd = os.environ.get("READTHEDOCS", None) == "True"
+
 
 # -- Project information -----------------------------------------------------
 
-project = 'Isogeo PySDK'
-author = 'Julien M. @ Isogeo'
-copyright = u'2016 - {0}, {1}'.format(date.today().year, author)
+project = "Isogeo PySDK"
+author = "Julien M. @ Isogeo"
+copyright = u"2016 - {0}, {1}".format(date.today().year, author)
 
 # The short X.Y version
 version = __version__
@@ -41,31 +46,28 @@ release = __version__
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.viewcode',
-              ]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ".rst"
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-#language = 'fr'
+# language = 'fr'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
-exclude_patterns = ["_build", "samples/*", "Thumbs.db", ".DS_Store",
-                    "*env*", "libs/*"]
+exclude_patterns = ["_build", "samples/*", "Thumbs.db", ".DS_Store", "*env*", "libs/*"]
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -76,26 +78,23 @@ pygments_style = 'sphinx'
 # html_theme = 'alabaster'
 
 if on_rtd:
-    html_theme = 'default'
+    html_theme = "default"
 else:
     import guzzle_sphinx_theme
-    html_theme = 'guzzle_sphinx_theme'
+
+    html_theme = "guzzle_sphinx_theme"
     html_theme_path = guzzle_sphinx_theme.html_theme_path()
     extensions.append("guzzle_sphinx_theme")
     # Guzzle theme options
     html_theme_options = {
         "project_nav_name": project,
-
         # Path to a touch icon
         "touch_icon": "",
-
         # Specify a base_url used to generate sitemap.xml links. If not
         # specified, then no sitemap will be built.
         "base_url": "http://isogeo-api-pysdk.readthedocs.io",
-
         # Allow a separate homepage from the master_doc
         # "homepage": "index",
-
         # Allow the project link to be overriden to a custom URL.
         "projectlink": "https://github.com/Isogeo/isogeo-api-py-minsdk/",
     }
@@ -119,7 +118,7 @@ else:
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'IsogeoPySDK_doc'
+htmlhelp_basename = "IsogeoPySDK_doc"
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -128,15 +127,12 @@ latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
-
     # The font size ('10pt', '11pt' or '12pt').
     #
     # 'pointsize': '10pt',
-
     # Additional stuff for the LaTeX preamble.
     #
     # 'preamble': '',
-
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
@@ -146,8 +142,13 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'IsogeoPySDK.tex', 'Isogeo PySDK Documentation',
-     'Isogeo PySDK', 'manual'),
+    (
+        master_doc,
+        "IsogeoPySDK.tex",
+        "Isogeo PySDK Documentation",
+        "Isogeo PySDK",
+        "manual",
+    )
 ]
 
 
@@ -155,10 +156,7 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, 'isogeopysdk', 'Isogeo PySDK Documentation',
-     [author], 1)
-]
+man_pages = [(master_doc, "isogeopysdk", "Isogeo PySDK Documentation", [author], 1)]
 
 
 # -- Options for Texinfo output ----------------------------------------------
@@ -167,9 +165,15 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'IsogeoPySDK', 'Isogeo PySDK Documentation',
-     author, 'IsogeoPySDK', 'Isogeo API Python SDK.',
-     'Miscellaneous'),
+    (
+        master_doc,
+        "IsogeoPySDK",
+        "Isogeo PySDK Documentation",
+        author,
+        "IsogeoPySDK",
+        "Isogeo API Python SDK.",
+        "Miscellaneous",
+    )
 ]
 
 
@@ -179,13 +183,12 @@ def run_apidoc(_):
     from sphinx.ext.apidoc import main
 
     cur_dir = os.path.normpath(os.path.dirname(__file__))
-    output_path = os.path.join(cur_dir, '_apidoc')
+    output_path = os.path.join(cur_dir, "_apidoc")
     modules = os.path.normpath(os.path.join(cur_dir, "../isogeo_pysdk"))
-    exclusions = [
-        '../isogeo_pysdk/samples',
-    ]
-    main(['-e', '-f', '-M', '-o', output_path, modules] + exclusions)
+    exclusions = ["../isogeo_pysdk/samples"]
+    main(["-e", "-f", "-M", "-o", output_path, modules] + exclusions)
+
 
 # launch setup
 def setup(app):
-    app.connect('builder-inited', run_apidoc)
+    app.connect("builder-inited", run_apidoc)
