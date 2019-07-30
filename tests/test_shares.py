9f931e5f44a69a0f940c39367921a444d1667d0a
Guts <Guts@users.noreply.github.com>
2019-07-30 20:21:49 +0200

    renaming catalogs methods

--- tests/test_shares.py
+++ tests/test_shares.py
@@ -174,7 +174,7 @@ class TestShares(unittest.TestCase):
     #         name="{} - {}".format(get_test_marker(), self.discriminator),
     #         type="application",
     #         catalogs= [
-    #             isogeo.catalog.catalog(workgroup_id=workgroup_test, "75a6e6b16026410999dc4153f16c7de2")
+    #             isogeo.catalog.get(workgroup_id=workgroup_test, "75a6e6b16026410999dc4153f16c7de2")
     #         ]
     #     )
     #     # create it online
