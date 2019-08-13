703d31262761c613d366d7c8e722f858cda4b046
Guts <Guts@users.noreply.github.com>
2019-08-13 18:32:24 +0200

    increase search coverage

--- tests/test_search.py
+++ tests/test_search.py
@@ -162,15 +162,158 @@ class TestSearch(unittest.TestCase):
     def test_search_specific_mds_bad(self):
         """Searches filtering on specific metadata."""
         # get random metadata within a small search
-        search = self.isogeo.search(
-            page_size=5,
-            # whole_share=0
-        )
-        metadata_id = sample(search.results, 2)
+        search = self.isogeo.search(whole_share=0)
+        metadata_id = sample(search.results, 1)[0]
 
-        # # pass metadata UUID
-        # with self.assertRaises(TypeError):
-        #     self.isogeo.search(self.bearer,
-        #                         page_size=0,
-        #                         whole_share=0,
-        #                         specific_md=md)
+        # pass metadata UUID
+        with self.assertRaises(TypeError):
+            self.isogeo.search(
+                page_size=0, whole_share=0, specific_md=metadata_id.get("_id")
+            )
+
+    # includes
+    def test_search_includes_ok(self):
+        """Searches including includes."""
+        self.isogeo.search(page_size=0, whole_share=0, include=["links", "contacts"])
+
+    def test_search_includes_all_ok(self):
+        """Searches including includes."""
+        self.isogeo.search(page_size=0, whole_share=0, include="all")
+
+    def test_search_includes_empty(self):
+        """Search with empty includes list."""
+        self.isogeo.search(page_size=0, whole_share=0, include=[])
+
+    def test_search_includes_bad(self):
+        """Include sub_resrouces require a list."""
+        with self.assertRaises(TypeError):
+            self.isogeo.search(page_size=0, whole_share=0, include="links")
+
+    # query
+    def test_search_parameter_query_ok(self):
+        """Search with good query parameters."""
+        # contacts
+        self.isogeo.search(
+            query="contact:group:643f1035377b4ca59da6f31a39704c34",
+            page_size=0,
+            whole_share=0,
+        )
+        self.isogeo.search(
+            query="contact:08b3054757544463abd06f3ab51ee491:fe3e8ef97b8446be92d3c315ccbc70f9",
+            page_size=0,
+            whole_share=0,
+        )
+        # catalog
+        self.isogeo.search(
+            query="catalog:633216a375ab48ca8ca72e4a1af7a266", page_size=0, whole_share=0
+        )
+        # CSW data-source
+        self.isogeo.search(
+            query="data-source:ace35ec171da4d0aa2f10e7308dcbdc5",
+            page_size=0,
+            whole_share=0,
+        )
+        # format
+        self.isogeo.search(query="format:shp", page_size=0, whole_share=0)
+        # has-no
+        self.isogeo.search(query="has-no:keyword", page_size=0, whole_share=0)
+        # inspire themes
+        self.isogeo.search(
+            query="keyword:inspire-theme:administrativeunits",
+            page_size=0,
+            whole_share=0,
+        )
+        # keyword
+        self.isogeo.search(query="keyword:isogeo:2018", page_size=0, whole_share=0)
+        # licenses
+        self.isogeo.search(
+            query="license:isogeo:63f121e14eda4f47b748595e0bcccc31",
+            page_size=0,
+            whole_share=0,
+        )
+        self.isogeo.search(
+            query="license:32f7e95ec4e94ca3bc1afda960003882:76c02a0baf594c77a569b3a1325aee30",
+            page_size=0,
+            whole_share=0,
+        )
+        # SRS
+        self.isogeo.search(query="coordinate-system:2154", page_size=0, whole_share=0)
+        # types
+        self.isogeo.search(query="type:dataset", page_size=0, whole_share=0)
+        self.isogeo.search(query="type:vector-dataset", page_size=0, whole_share=0)
+        self.isogeo.search(query="type:raster-dataset", page_size=0, whole_share=0)
+        self.isogeo.search(query="type:service", page_size=0, whole_share=0)
+        self.isogeo.search(query="type:resource", page_size=0, whole_share=0)
+        # workgroup - owner
+        self.isogeo.search(
+            query="owner:32f7e95ec4e94ca3bc1afda960003882", page_size=0, whole_share=0
+        )
+        # unknown
+        self.isogeo.search(query="unknown:filter", page_size=0, whole_share=0)
+
+        def test_search_bad_parameter_query(self):
+            """Search with bad parameter."""
+            with self.assertRaises(ValueError):
+                self.isogeo.search(query="type:youpi")
+            with self.assertRaises(ValueError):
+                self.isogeo.search(query="action:yipiyo")
+            with self.assertRaises(ValueError):
+                self.isogeo.search(query="provider:youplaboum")
+
+        def test_search_bad_parameter_geographic(self):
+            """Search with bad parameter."""
+            # geometric operator
+            with self.assertRaises(ValueError):
+                # georel should'nt be used without box or geo
+                self.isogeo.search(georel="intersects")
+            with self.assertRaises(ValueError):
+                # georel bad value
+                self.isogeo.search(bbox="-4.970,30.69418,8.258,51.237", georel="cross")
+
+        def test_parameter_not_unique_search(self):
+            """SDK raises error for search with a parameter that must be unique."""
+            with self.assertRaises(ValueError):
+                self.isogeo.search(
+                    query="coordinate-system:32517 coordinate-system:4326"
+                )
+            with self.assertRaises(ValueError):
+                self.isogeo.search(query="format:shp format:dwg")
+            with self.assertRaises(ValueError):
+                self.isogeo.search(
+                    query="owner:32f7e95ec4e94ca3bc1afda960003882 owner:08b3054757544463abd06f3ab51ee491"
+                )
+            with self.assertRaises(ValueError):
+                self.isogeo.search(query="type:vector-dataset type:raster-dataset")
+            # disabling check, it should not raise anything
+            self.isogeo.search(
+                query="coordinate-system:32517 coordinate-system:4326", check=0
+            )
+            self.isogeo.search(query="format:shp format:dwg", check=0)
+            self.isogeo.search(
+                query="owner:32f7e95ec4e94ca3bc1afda960003882 owner:08b3054757544463abd06f3ab51ee491",
+                check=0,
+            )
+            self.isogeo.search(query="type:vector-dataset type:raster-dataset", check=0)
+
+    # search utilities
+    def test_search_augmented(self):
+        """Augmented search with shares UUID"""
+        # at start, shares_id attribute doesn't exist
+        self.assertFalse(hasattr(self.isogeo, "shares_id"))
+        # normal
+        search = self.isogeo.search(page_size=0, whole_share=0, augment=0)
+        tags_shares = [i for i in search.tags if i.startswith("share:")]
+        # shares_id attribute still doesn't exist
+        self.assertEqual(len(tags_shares), 0)
+        self.assertFalse(hasattr(self.isogeo, "shares_id"))
+
+        # augment it
+        search = self.isogeo.search(page_size=0, whole_share=0, augment=1)
+
+        # compare
+        tags_shares = [i for i in search.tags if i.startswith("share:")]
+        self.assertNotEqual(len(tags_shares), 0)
+        self.assertTrue(hasattr(self.isogeo, "shares_id"))  # now it exists
+
+        # redo using existing attribute
+        search = self.isogeo.search(page_size=0, whole_share=0, augment=1)
