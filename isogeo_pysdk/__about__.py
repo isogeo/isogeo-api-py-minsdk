63f54112d3ff89f74d66888628488960b3cc70ea
Julien <Guts@users.noreply.github.com>
2019-08-26 13:57:05 +0200

    Update Docs to fit version 3.2 (#92)
    * minor clean up in docstring
    * remove outdated content

--- isogeo_pysdk/__about__.py
+++ isogeo_pysdk/__about__.py
@@ -1,6 +1,8 @@
 # Metadata bout the package to easily retrieve informations about it.
 # see: https://packaging.python.org/guides/single-sourcing-package-version/
 
+from datetime import date
+
 __all__ = [
     "__title__",
     "__summary__",
@@ -17,10 +19,10 @@ __title__ = "Isogeo Python SDK"
 __summary__ = "API wrapper for the Isogeo REST API"
 __uri__ = "https://pypi.org/project/isogeo-pysdk/"
 
-__version__ = "3.1.0"
+__version__ = "3.2.0"
 
 __author__ = "Isogeo"
 __email__ = "contact@isogeo.com"
 
 __license__ = "GNU Lesser General Public License v3.0"
-__copyright__ = "2016 %s" % __author__
+__copyright__ = "2016 - {0}, {1}".format(date.today().year, __author__)
