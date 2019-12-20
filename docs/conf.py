dbdfede5e4c8409cd6025dd2a34b7cfd15aec47e
Guts <Guts@users.noreply.github.com>
2019-12-20 10:29:35 +0100

    Use markdown for documentation #138

--- docs/conf.py
+++ docs/conf.py
@@ -48,13 +48,19 @@ release = __version__
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.intersphinx", "sphinx.ext.viewcode"]
+extensions = [
+    "recommonmark",
+    "sphinx.ext.autodoc",
+    "sphinx_click.ext",
+    "sphinx_markdown_tables",
+    "sphinx_rtd_theme",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
 # source_suffix = ['.rst', '.md']
-source_suffix = ".rst"
+source_suffix = {".rst": "restructuredtext", ".md": "markdown"}
 
 # The master toctree document.
 master_doc = "index"
